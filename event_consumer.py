8d1707b2e4c07bac9a00420e15b7afc5034504be
Najeem Adeleke <nadeleke@gmail.com>
2017-09-28 13:43:14 -0400

    scripts

---
+++ event_consumer.py
@@ -0,0 +1,380 @@
+from __future__ import print_function
+import sys
+import ast
+import redis
+import datetime
+import numpy as np
+
+from pyspark import SparkContext
+# from pyspark.sql.types import StructType, IntegerType, StringType, StructField
+from pyspark.streaming import StreamingContext
+from pyspark.sql import SparkSession, SQLContext, Row, DataFrame
+# from pyspark.sql import DataFrameReader
+from pyspark.streaming.kafka import KafkaUtils
+
+u = 'ubuntu'
+p = 'ubuntu'
+db = 'ubuntudb'
+host = 'localhost'
+port = "5432"
+
+
+# # Lazily instantiated global instance of SparkSession
+# def getSparkSessionInstance(sparkConf):
+#     if ("sparkSessionSingletonInstance" not in globals()):
+#         globals()["sparkSessionSingletonInstance"] = SparkSession \
+#             .builder \
+#             .config(conf=sparkConf) \
+#             .getOrCreate()
+#     return globals()["sparkSessionSingletonInstance"]
+#
+#
+# def process(rdd):
+#     try:
+#         # Get the singleton instance of SparkSession
+#         spark = getSparkSessionInstance(rdd.context.getConf())
+#
+#         df = spark.read.json(rdd)
+#
+#         # Creates a temporary view using the DataFrame
+#         df.createOrReplaceTempView("temp")
+#         # df.show()
+#         sqlDF = spark.sql("SELECT id as wellid, field_name as field, dt, wellbore_vol as prod_vol FROM temp")
+#         sqlDF.show()
+#
+#         sqlDF.write \
+#             .format("org.apache.spark.sql.cassandra") \
+#             .mode('append') \
+#             .options(table="test", keyspace="downtime_estimation") \
+#             .save()
+#     except:
+#         pass
+
+
+# def write_to_postres_DB(rdd):
+#     try:
+#         # Get the singleton instance of SparkSession
+#         spark = getSparkSessionInstance(rdd.context.getConf())
+#
+#         df = spark.read.json(rdd)
+#
+#         # Creates a temporary view using the DataFrame
+#         df.createOrReplaceTempView("temp")
+#         # df.show()
+#
+#         sqlDF = spark.sql("SELECT id, vp_hist, time_hist FROM temp")
+#         sqlDF.show()
+#
+#         # sqlDF.write \
+#         #     .format("org.postgresql_postgresql-42.1.1") \
+#         #     .mode('append') \
+#         #     .options(table="test", keyspace="downtime_estimation") \
+#         #     .save()
+#     except:
+#         pass
+
+
+def getEvents(tokens):
+    # dt = datetime.datetime.strptime(tokens[0], "%Y-%m-%d %H:%M:%S")
+    dt = tokens[0].decode()
+    ID = tokens[1].decode()
+    status = tokens[2].decode()
+    vol_prod = tokens[3].decode()
+    fieldID = tokens[4].decode()
+    completion_type = tokens[5].decode()
+    wellbore_vol = tokens[6].decode()
+    field_name = tokens[7].decode()
+    state = tokens[8].decode()
+    rate = tokens[9].decode()
+    return {'id': ID, 'time': dt, 'status': status, 'vol_prod': vol_prod, 'fieldID': fieldID,
+            'completion_type': completion_type, 'wellbore_vol': wellbore_vol, 'field_name': field_name, 'state': state,
+            'rate': rate}  # Note: the rate here is production per day and not
+
+
+def getSeconds(t):
+    t0 = datetime.datetime.strptime(t, "%Y-%m-%d %H:%M:%S")
+    return (t0 - datetime.datetime(1870, 1, 1)).total_seconds()
+
+
+def seconds_between(d1, d2):
+    d1 = datetime.datetime.strptime(d1, "%Y-%m-%d %H:%M:%S")
+    d2 = datetime.datetime.strptime(d2, "%Y-%m-%d %H:%M:%S")
+    return abs((d2 - d1).total_seconds())
+
+
+def sortEvent(x, y):
+    x1 = datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
+    y1 = datetime.datetime.strptime(y, "%Y-%m-%d %H:%M:%S")
+    if x1 < y1:
+        earlier = x
+    else:
+        earlier = y
+    return earlier
+    # return x
+
+
+def solveLinearSys(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    return np.linalg.solve(a, b)
+
+
+def call_estimator_looper(rdd):
+    for i in rdd:
+        call_estimator(i)
+
+
+def call_estimator(rdd):
+    id = rdd['id']
+    time = rdd['time']
+    status = rdd['status']
+    vol_p = rdd['vp']
+    vol_w = rdd['vw']
+    # rp = rdd['rop']
+    field_id = rdd['fieldID']
+    comp_type = rdd['comp']
+    field = rdd['field_name']
+    # state = rdd['state']
+
+    ## point to hist_map in redis cache
+    # hist_map = redis.StrictRedis(host='ec2-34-234-123-146.compute-1.amazonaws.com', port=6379, db=0,
+    #                              decode_responses=True)
+
+    ## point to hist_map in distibuted redis cache
+    hist_map = redis.StrictRedis(host='ec2-52-54-82-142.compute-1.amazonaws.com', port=6379, db=0,
+                                 decode_responses=True)
+
+
+    # Obtain well recent history from redis cache
+    history = hist_map.hgetall(id)
+
+    # read from redis for recent history data or set initial values
+    if history == {}:
+
+        if hist_map.keys():
+            # if redis is empty compute field averages for initialization parameters
+            vp_hist_sum = [0, 0, 0, 0]
+            on_dt_hist_sum = [0, 0, 0, 0]
+            off_dt_hist_sum = [0, 0, 0, 0]
+            count = 0
+            for temp_id in hist_map.keys():
+                history_avg = hist_map.hgetall(temp_id)
+                hav1 = ast.literal_eval(history_avg['vp'])
+                hav2 = ast.literal_eval(history_avg['on_list'])
+                hav3 = ast.literal_eval(history_avg['off_list'])
+                if field_id == ast.literal_eval(history_avg['fieldID']) and comp_type == ast.literal_eval(
+                        history_avg['comp']):
+                    if (len(hav1) + len(hav2) + len(hav3)) == 12:
+                        count += 1
+                        vp_hist_sum = [x + y for x, y in zip(vp_hist_sum, hav1)]
+                        on_dt_hist_sum = [x + y for x, y in zip(on_dt_hist_sum, hav2)]
+                        off_dt_hist_sum = [x + y for x, y in zip(off_dt_hist_sum, hav3)]
+
+            if count > 0:
+                vp_hist = [x / count for x in vp_hist_sum]
+                on_dt_hist = [x / count for x in on_dt_hist_sum]
+                off_dt_hist = [x / count for x in off_dt_hist_sum]
+                time_hist = [
+                    datetime.datetime.strptime(time.decode(), "%Y-%m-%d %H:%M:%S") - datetime.timedelta(seconds=60)]
+            else:
+                # Initializing empty list if hist_map on redis is empty for the current wellID
+                time_hist = []
+                vp_hist = []
+                on_dt_hist = []
+                off_dt_hist = []
+        else:
+            # Initializing empty list if hist_map on redis is empty for the current wellID
+            time_hist = []
+            vp_hist = []
+            on_dt_hist = []
+            off_dt_hist = []
+
+    else:
+        time_hist = ast.literal_eval(history['time'])
+        vp_hist = ast.literal_eval(history['vp'])
+        on_dt_hist = ast.literal_eval(history['on_list'])
+        off_dt_hist = ast.literal_eval(history['off_list'])
+        if status == history['status']:
+            if status == 'on':
+                history['status'] = 'off'
+                if vp_hist:
+                    vp_hist.pop(len(vp_hist) - 1)
+                    off_dt_hist.pop(len(off_dt_hist) - 1)
+            else:
+                history['status'] = 'on'
+                if vp_hist:
+                    vp_hist.pop(len(vp_hist) - 1)
+                    on_dt_hist.pop(len(on_dt_hist) - 1)
+
+    # --------------------------
+    # The simple model
+    # --------------------------
+    # # Determine delta_t
+    # delta_t = seconds_between(time, time_hist[-1])
+    #
+    # # Estimated inflow rate
+    # r_inflow = vol_p / delta_t - vol_w / delta_t
+    #
+    # # downtime estimate
+    # delta_t_n = r_inflow / vol_w
+    #
+    # # determine estimated time for next start up
+    # time_n = datetime.timedelta(seconds=delta_t_n) + datetime.datetime.strptime(time.decode(), "%Y-%m-%d %H:%M:%S")
+    #
+    # # update redis
+    # hist_map.hmset(id, {'id': id, 'status': status, 'time': time_hist, 'vp': vp_hist, 'on_list': on_dt_hist,
+    #                 'off_list': off_dt_hist, 'time_n': time_n, 'delta_t_n': delta_t_n, 'fieldID': field_id,
+    #                 'comp': comp_type, 'field': field})
+    #
+    # return hist_map.hgetall(id)
+
+
+
+    # --------------------------
+    # The comprehensive model
+    # --------------------------
+    if len(vp_hist) >= 3:  # Determine downtown estimate
+
+        # determine delta_t
+        delta_t = seconds_between(time, time_hist[-1])
+
+        # append time and production volume
+        time_hist.append(time)
+
+        if status == 'off':  # update on_list and estimate downtime
+
+            on_dt_hist.append(delta_t)
+            vp_hist.append(vol_p)
+            if len(on_dt_hist) > 4:
+                on_dt_hist.pop(0)
+                vp_hist.pop(0)
+
+            # Determine start up time estimate
+            r = [x / y for x, y in zip(vp_hist[-3:], on_dt_hist[-3:])]
+
+            # Estimated inflow rate
+            try:
+                cc = solveLinearSys([[r[0], 1], [r[1], 1]], [r[1], r[2]])
+                rn = cc[0] * r[-1] + cc[1]
+                r_inflow = 0.5 * (rn + vol_p / delta_t) - vol_w / delta_t
+                # r_inflow = rn - vol_w / delta_t
+            except:
+                r_inflow = vol_p / delta_t - vol_w / delta_t
+
+            # # Estimated inflow rate
+            # r_inflow = vol_p / delta_t - vol_w / delta_t
+
+            # downtime estimate
+            delta_t_n = r_inflow / vol_w
+
+            # determine estimated time for next start up
+            time_n = datetime.timedelta(seconds=delta_t_n) + datetime.datetime.strptime(time.decode(),
+                                                                                        "%Y-%m-%d %H:%M:%S")
+
+        else:  # update off_list and set identifier
+
+            # update off list
+            off_dt_hist.append(delta_t)
+            if len(off_dt_hist) > 4:
+                off_dt_hist.pop(0)
+
+            # 'on' status identifier
+            time_n = -777
+            delta_t_n = -777
+
+        # check list lengths and pop unneeded history to save memory space
+        if len(time_hist) > 7:
+            time_hist.pop(0)
+            # vp_hist.pop(0)
+
+        # update redis cache
+        hist_map.hmset(id, {'id': id, 'status': status, 'time': time_hist, 'vp': vp_hist, 'on_list': on_dt_hist,
+                            'off_list': off_dt_hist, 'time_n': time_n, 'delta_t_n': delta_t_n, 'fieldID': field_id,
+                            'comp': comp_type, 'field': field})
+
+    else:
+        # make rough estimate, do updates and set identifiers
+        if time_hist:
+            if status == 'off':
+                # determine delta_t
+                delta_t = seconds_between(time, time_hist[-1])
+                # update production volume list
+                vp_hist.append(vol_p)
+                # update on_list
+                on_dt_hist.append(delta_t)
+                # time_n = datetime.timedelta(seconds=delta_t) + datetime.datetime.strptime(time.decode(), "%Y-%m-%d %H:%M:%S")
+                # delta_t_n = -2
+
+                # Inflow rate
+                r_inflow = vol_p / delta_t - vol_w / delta_t
+
+                # downtime estimate
+                delta_t_n = r_inflow / vol_w
+
+                # Estimate up time
+                time_n = datetime.timedelta(seconds=delta_t) + datetime.datetime.strptime(time.decode(),
+                                                                                          "%Y-%m-%d %H:%M:%S")
+            else:
+                # determine delta_t
+                delta_t = seconds_between(time, time_hist[-1])
+                # update on_list
+                off_dt_hist.append(delta_t)
+                time_n = -1
+                delta_t_n = -1
+        else:
+            time_n = -999
+            delta_t_n = -999
+
+        # update recent timestamp list
+        time_hist.append(time)
+
+        # update redis
+        hist_map.hmset(id, {'id': id, 'status': status, 'time': time_hist, 'vp': vp_hist, 'on_list': on_dt_hist,
+                            'off_list': off_dt_hist, 'time_n': time_n, 'delta_t_n': delta_t_n, 'fieldID': field_id,
+                            'comp': comp_type, 'field': field})
+
+    return hist_map.hgetall(id)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: streaming <bootstrap.servers>", file=sys.stderr)
+        exit(-1)
+
+    kafkaIP = str(sys.argv[1])
+
+    sc = SparkContext(appName="event")
+    sc.setLogLevel("WARN")
+    ssc = StreamingContext(sc, 1)
+
+    # kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'event_data_topic': 1})
+    # kafkaStream = KafkaUtils.createStream(ssc, kafkaIP + ':9092', 'spark-streaming', {'event_data_topic': 1})
+    # kafkaStream = KafkaUtils.createDirectStream(ssc, ["event_data_topic"], {"bootstrap.servers": '54.156.225.243' + ':9092'})
+    kafkaStream = KafkaUtils.createDirectStream(ssc, ["event_data_topic"], {"bootstrap.servers": kafkaIP + ':9092'})
+
+    # counting number of messages in kafka stream
+    kafkaStream.count().map(lambda x: 'Messages in this batch: %s' % x).pprint()
+
+    # extracting data and counting
+    tuple_rdd = kafkaStream.map(lambda x: x[1])
+    # tuple_rdd.pprint()
+
+    # converting 'string of tuple' to tuple and extracting string of dictionary
+    json_string_rdd = tuple_rdd.map(lambda x: ast.literal_eval(x)[1])
+    # json_rdd.pprint()
+
+    # Convert 'string of dictionary' to dictionary
+    json_rdd = json_string_rdd.map(lambda x: ast.literal_eval(x))
+    # json_rdd.pprint()
+
+    # json_rdd.foreachRDD(lambda rdd: rdd.foreachPartition(lambda x: call_estimator(x)))
+    # result = json_rdd.map(lambda x: call_estimator(x))
+    # json_rdd.foreachRDD(lambda x: x.foreachPartition(lambda y: call_estimator_looper(y)))
+    # json_rdd.foreachRDD(lambda x: call_estimator(x))
+    # result.pprint()
+    json_rdd.map(lambda x: call_estimator(x))
+
+
+    ssc.start()
+    ssc.awaitTermination()
---
+++ event_producer.py
@@ -0,0 +1,283 @@
+import sys
+import ast
+import csv
+import time
+import json
+import random
+import datetime
+from kafka.client import SimpleClient
+from kafka.producer import KeyedProducer
+
+
+# --------------------------------
+# Creating Fields
+# --------------------------------
+def readFieldData(file_name):
+    # Loading field info
+    fields = {}
+    with open(file_name) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            fields[row['index']] = [row['field_name'], row['state'], row['size_(MM_Bbls)']]
+            # print(row['index'], row['field_name'], row['state'], row['size_(MM_Bbls)'])
+    # print(fields)
+    return fields
+
+
+# --------------------------------
+# Creating unique wells
+# --------------------------------
+def genWellInfo(fields, numOfWells):
+    well_info = {}
+
+    while len(well_info) < numOfWells:
+        wellID = 0.0 + random.randrange(100000, numOfWells+300000)  # 1E6)
+        # wellID = 0.0 + random.randrange(100, numOfWells + 120)  # 1E6)
+
+        if wellID not in well_info:
+            # Assigning a random completion type
+            r = random.randrange(0, len(completion_types))
+            completion_type = completion_types[r]
+            # Assigning completion type properties
+            if completion_type == 'BP':
+                pay_height = random.choice([100, 120])  # ft
+                # rate_prod = random.randrange(100, 200)  # BBls/d
+                pump_rate = random.randrange(50000, 500000)  # Bbls/s
+            elif completion_type == 'ESP':
+                pay_height = random.randrange(120, 300)  # ft
+                # rate_prod = random.randrange(150, 350)  # Bbls/d
+                pump_rate = random.randrange(0.7E6, 15E6)  # Bbls/s
+
+            # Assigning random well diameter
+            well_diam = random.randrange(2, 7)  # in inches
+            well_diam = well_diam * 0.0833333  # ft -- converting inches to ft
+
+            # Calculating well volume
+            wellbore_vol = 3.14/4.0 * well_diam * well_diam * pay_height / 5.61458333  # Bbls
+
+            # Assigning a random field to this wellID
+            fieldID = str(random.randrange(1, len(fields) + 1))
+
+            # Assigning field specific data to this wellID
+            field_name = fields[fieldID][0]
+            state = fields[fieldID][1]
+            size = fields[fieldID][2]
+
+            well_info[wellID] = [fieldID, completion_type, wellbore_vol, field_name, state, size, pump_rate]
+
+    # print('Well info =', well_info)
+    return well_info
+
+
+# --------------------------------------
+# Assigning initial well status randomly
+# --------------------------------------
+def genWellStatus(well_info):
+    well_stat = {}
+    for wellID in well_info:
+        event = event_types[random.randrange(0, len(event_types))]
+        well_stat[wellID] = event
+
+    # print('well_stat =', well_stat)
+    return well_stat
+
+
+# -------------------------------------------
+# This function generates the next time stamp
+# -------------------------------------------
+def nextTimeStamp(dt, maxStep):
+    # Generating random datetime step (on second scale)
+    r = random.randrange(max(1, max_step-10), maxStep)
+    step = datetime.timedelta(seconds=r)
+    return dt + step
+
+
+# ---------------------------------------------
+# Generating single event data
+# ---------------------------------------------
+def genEvent(dt, wellID, well_stat, well_info, last_event_data=None):
+
+    fieldID = well_info[wellID][0]
+    completion_type = well_info[wellID][1]
+    wellbore_vol = well_info[wellID][2]
+    field_name = well_info[wellID][3]
+    state = well_info[wellID][4]
+    size = well_info[wellID][5]
+    pump_rate = well_info[wellID][6]
+
+    if well_stat[wellID] is event_types[0]:
+        event = event_types[1]
+        well_stat[wellID] = event_types[1]
+    else:
+        event = event_types[0]
+        well_stat[wellID] = event_types[0]
+
+    if event == 'on':  # no production prior to event
+        vol_prod = 0
+    else:  # event = 'off' so determine production
+        if last_event_data is not None:
+            time0 = ast.literal_eval(last_event_data[1])['time']
+            # time0 = json.loads(last_event_data[1])['time']
+            # print(last_event_data)
+            # print(last_event_data[1])
+            # print(ast.literal_eval(last_event_data[1])['time'])
+            # input('pause')
+
+            vol_prod = (dt - datetime.datetime.strptime(str(time0), "%Y-%m-%d %H:%M:%S")).total_seconds() * pump_rate  # Bbls
+            # minimum production
+            if vol_prod < 0.7 * wellbore_vol:
+                vol_prod = 0.7 * wellbore_vol
+        else:
+            vol_prod = 0.7 * wellbore_vol  # wellbore_vol
+
+    # percent of wellbore volume producible
+    producible_wv = 0.7 * wellbore_vol  # Bbls
+
+    # str_fmt = "{};{};{};{};{};{};{};{};{};{}"
+    # reform_event_data = str_fmt.format(str(dt), str(wellID), event, vol_prod, fieldID, completion_type, wellbore_vol, field_name, state, size)
+
+    reform_event_data = dict(id=str(wellID), time=str(dt), status=event, vp=vol_prod, vw=producible_wv, \
+                             fieldID=fieldID, field_name=field_name, comp=completion_type, rop=pump_rate, \
+                             state=state, size=int(size))
+
+    return well_stat, (wellID, str(reform_event_data))
+
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d %H:%M:%S")
+    d2 = datetime.strptime(d2, "%Y-%m-%d %H:%M:%S")
+    return abs((d2 - d1).seconds)
+
+
+if __name__ == "__main__":
+
+    args = sys.argv
+    ip_address = str(args[1])
+    partition_key = str(args[2])
+
+    print(args, '\n', 'ip address -->', ip_address)
+    # input('p')
+
+    client = SimpleClient(ip_address)
+    producer = KeyedProducer(client)
+
+    # --------------------------------
+    # Defining data variables
+    # --------------------------------
+    dt = datetime.datetime(2017, 1, 22, 9, 53, 42)  # Defining a starting date and time
+    max_step = 20  # Time step max increment size (it's a random fxn btw 0 and max_step)
+    numOfWells = 100000  # Number of wells
+    # n = 700  # Number of event data points
+
+    # --------------------------------
+    # Generating time series data
+    # --------------------------------
+    event_types = ['on', 'off']
+    completion_types = ['BP', 'ESP']  # diff from 'completion_type' (no s)
+
+    # --------------------------------
+    # creating instance of producer and generating data
+    # --------------------------------
+
+    # Loading field Data
+    fields = readFieldData('field.csv')
+
+    # Generating Well and Event Data
+    well_info = genWellInfo(fields, numOfWells)
+    well_stat = genWellStatus(well_info)
+
+    # Seperating wells by fields
+    wells_by_field = {}
+    for i in range(1, len(fields) + 1):
+        wells_by_field[i] = []
+
+    for wellID in well_info:
+        for i in range(1, len(fields)+1):
+            if well_info[wellID][3] == fields[str(i)][0]:
+                wells_by_field[i].append(wellID)
+
+
+    # Determining max number of wells in a field
+    max_n_wells = 0
+    for i in range(1, len(fields) + 1):
+        n = len(wells_by_field[i])
+        if n > max_n_wells:
+            max_n_wells = n
+        print(n, 'wells in', fields[str(i)][0], 'field')
+
+
+    # last event data
+    last_event_data = {}  # or load data from file
+    for wellID in well_info:
+        last_event_data[wellID] = None
+
+    # ----------------------------------
+    # Generating time series event data
+    # ----------------------------------
+    cycle_count = 0
+    # well range for j in range (jf, jfn)
+    jf = {}     # field index
+    jfn = {}    # field index
+    for i in range(1, len(fields) + 1):
+        jfn[i], jf[i] = 0, 0
+
+    # while dt < datetime.datetime(2017, 1, 22, 9, 53, 42):
+    while True:
+
+        # Wait for 5 seconds
+        time.sleep(1)
+
+        dt = nextTimeStamp(dt, max_step)  # increase max_step to extend
+
+        for i in range(1, len(fields) + 1):  # for every new timestamp, a random number of wells per field have an event
+
+            r = random.randrange( 1, max(2, int(int(fields[str(i)][2]) / .1) ) )
+            jfn[i] = jf[i] + r
+
+            if jfn[i] > len(wells_by_field[i]):
+                jfn[i] = len(wells_by_field[i])
+
+        wells_per_cycle = 0
+        for i in range(1, len(fields) + 1):  # field loop
+
+            for j in range(jf[i], jfn[i]):  # next set of wells in the field loop
+                # time.sleep(.2)
+                wells_per_cycle += 1
+
+                if len(wells_by_field[i]) != 0 :
+
+                    # print('here', i, j, jf[i], jfn[i])
+
+                    wellID = wells_by_field[i][j]
+
+                    # print('b4=', well_stat[wellID])
+                    # print('lastwelldata = ', last_event_data[wellID] )
+
+                    # Generate event
+                    well_stat, event = genEvent(dt, wellID, well_stat, well_info, last_event_data[wellID])
+
+                    # Update last event dictionary
+                    last_event_data[wellID] = event
+
+                    # Generating historical data
+                    # with open('historical_data.txt', 'a') as f:
+                    #     f.write(str(event)+'\n')
+
+                    print(str(event))
+                    # print('after=',  well_stat[wellID])
+                    producer.send_messages('event_data_topic', str(partition_key).encode(), str(event).encode())
+
+                    if jfn[i] >= len(wells_by_field[i]):
+                        # print('here2', i, j, jf[i], jfn[i])
+                        # print(jf[i], jfn[i])
+                        # input('pause')
+                        jfn[i] = 0
+                        jf[i] = 0
+
+            jf[i] = jfn[i]
+            # input('pause')
+
+        # print(wells_per_cycle, ' wells in this batch', )
+        # print('------------------------------------------------------------------------------------------')
+
+        cycle_count += 1
