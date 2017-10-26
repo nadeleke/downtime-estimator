50709b64fa8146333403950954617d4988357bc3
Najeem Adeleke <nadeleke@gmail.com>
2017-10-26 19:36:27 -0400

    house keeping

--- src/submit_spark_process.sh
+++ src/submit_spark_process.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-#PYSCRP=$1
-#SPARK_IP_ADDR=$2
-#KAFKA_MASTER=$3
-#spark-submit --master "spark://$SPARK_IP_ADDR:7077" --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2  $PYSCRP $KAFKA_MASTER
-
-PYSCRP=$1
-SPARK_IP_ADDR=$2
-KAFKA_MASTER=$3
-spark-submit --master "spark://$SPARK_IP_ADDR:7077" --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2   $PYSCRP $KAFKA_MASTER
-
-#PY_SCRP=$1
-#SPARK_MASTER=$2
-#KAFKA_MASTER=$3
-#CASSANDRA_WORKERS=$4
-#spark-submit --master spark://$SPARK_MASTER:7077 --conf spark.cassandra.connection.host=$CASSANDRA_WORKERS --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0,datastax:spark-cassandra-connector:2.0.5-s_2.11 $PY_SCRP $KAFKA_MASTER
-
-#PY_SCRP=$1
-#SPARK_MASTER=$2
-#KAFKA_MASTER=$2
-#POSTGRES_IP=$3
-#spark-submit --master spark://$SPARK_MASTER:7077 --conf spark.cassandra.connection.host=$POSTGRES_IP --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0,datastax:spark-cassandra-connector:2.0.5-s_2.11 $PY_SCRP $KAFKA_MASTER
-
+PYTHON_SCRP=$1
+SPARK_MASTER_IP=$2
+KAFKA_MASTER_IP=$3
+REDIS_MASTER_IP=$4
+spark-submit --master "spark://$SPARK_MASTER_IP:7077" --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2,org.postgresql:postgresql:42.1.1,com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:2.7.3 --driver-class-path ~/.ivy2/jars/org.postgresql_postgresql-42.1.1.jar $PYTHON_SCRP $KAFKA_MASTER_IP $REDIS_MASTER_IP
 
 
